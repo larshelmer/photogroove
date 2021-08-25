fa9d3fda252a99c6ea4d0f22cc1245bce4fc1e7e
lars helmer <lars.helmer@qlik.com>
2021-08-25 17:29:51 +0200

    3.2.3

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,18 +1,113 @@
 module PhotoGroove exposing (main)
 
+import Array exposing (Array)
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
+
+type alias Msg =
+    { description : String, data : String }
+
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+            , button
+                [ onClick { description = "ClickedSurpriseMe", data = "" } ]
+                [ text "Surprise Me!" ]
+            , h3 [] [ text "Thumbnail Size:" ]
+            , div [ id "choose-size" ]
+                (List.map viewSizeChooser [ Small, Medium, Large ])
+            , div [ id "thumbnails", class (sizeToString model.chosenSize) ]
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
             ]
+            []
         ]
 
+viewThumbnail : String -> Photo -> Html Msg
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        , onClick { description = "ClickedPhoto", data = thumb.url }
+        ]
+        []
+
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+    label []
+        [ input [ type_ "radio", name "size" ] []
+        , text (sizeToString size)
+        ]
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+        Medium ->
+            "med"
+        Large ->
+            "large"
+
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
+
+type alias Photo =
+    { url : String }
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
+    , chosenSize : ThumbnailSize
+    }
+
+initialModel : Model
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    , chosenSize = Medium
+    }
+
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+getPhotoUrl : Int -> String
+getPhotoUrl index =
+    case Array.get index photoArray of
+        Just photo ->
+            photo.url
+        Nothing ->
+            ""
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg.description of
+        "ClickedPhoto" ->
+            { model | selectedUrl = msg.data }
+        "ClickedSurpriseMe" ->
+            { model | selectedUrl = "2.jpeg" }
+        _ ->
+            model
 
 main =
-    view "no model yet"
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
